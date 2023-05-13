6b8640d9da07b415b82e5e4fe1cc6efb2ba7181e
whippo <whippocode@gmail.com>
2023-05-13 19:13:06 -0300

    cambio de banner pricipal

--- src/component/Slider.jsx
+++ src/component/Slider.jsx
@@ -8,7 +8,7 @@ function Slider() {
       <div className="carousel-inner">
         <div className="carousel-item active">
           <img
-            src="https://res.cloudinary.com/dm4wfkipp/image/upload/v1675546463/banner-blanco_ffjvn1.jpg"
+            src="https://res.cloudinary.com/dm4wfkipp/image/upload/v1684015468/unnamed_uhzytk.jpg"
             className="d-block w-100 rounded m-5"
             alt="..."
           />
